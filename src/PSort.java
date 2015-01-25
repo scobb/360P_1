ba87d12c26f972148587f2cb768918ac1380af78
Steve Cobb <scobb@alumni.lsu.edu>
2015-01-25 14:18:08 -0600

    Initial commit.

---
+++ src/PSort.java
@@ -0,0 +1,18 @@
+
+public class PSort {
+	// should use runnable.
+	public static void parallelSort(int[] a, int begin, int end){
+		
+	}
+	
+	public class QuickSortRunnable implements Runnable {
+
+		@Override
+		public void run() {
+			// this is where we should sort.
+			
+		}
+		
+	}
+
+}
