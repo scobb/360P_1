20eae9eef1096c86f361eed87cd5604a2a4d13f2
Steve Cobb <scobb@alumni.lsu.edu>
2015-01-26 11:46:17 -0600

    Using an executor service.

--- src/PSearch.java
+++ src/PSearch.java
@@ -1,6 +1,10 @@
 import java.util.ArrayList;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class PSearch {
 	public static class SearchCallable implements Callable<Integer> {
@@ -31,33 +35,43 @@ public class PSearch {
 
 	public static int parallelSearch(int x, int[] A, int numThreads) {
 		// need to use Callable
+		ExecutorService executorService = Executors
+				.newFixedThreadPool(numThreads);
 		int num_per_thread = A.length / numThreads;
-		ArrayList<Integer> results = new ArrayList<Integer>();
+		int remainder = A.length - num_per_thread;
+		ArrayList<Future<Integer>> results = new ArrayList<Future<Integer>>();
 		int start_ind = 0;
 		for (int i = 0; i < numThreads; i++) {
 			try {
-				if (i == numThreads - 1) {
-					results.add((new SearchCallable(A, start_ind, A.length, x))
-							.call());
-				} else {
-					results.add((new SearchCallable(A, start_ind, start_ind
-							+ num_per_thread, x)).call());
+				int end_ind = start_ind + num_per_thread;
+				if (remainder > 0) {
+					remainder--;
+					end_ind++;
 				}
+				results.add(executorService.submit(new SearchCallable(A,
+						start_ind, end_ind, x)));
 				start_ind += num_per_thread;
 			} catch (Exception exc) {
 
 			}
 		}
-		for (int result: results) {
-			if (result != -1)
-				return result;
+		for (Future<Integer> result : results) {
+			try {
+				int index = result.get();
+				if (index != -1) {
+					return index;
+				}
+			} catch (Exception exc) {
+				//TODO - handle ??
+
+			}
 		}
 		return -1;
 	}
-	
+
 	public static void main(String[] args) {
 		int[] arr = { 14, 12, 16, 1, 3, 18, 7, 76 };
-		int target = 100;
+		int target = 16;
 		int num_threads = 4;
 		int result = PSearch.parallelSearch(target, arr, num_threads);
 		System.out.println("Result: " + result);
